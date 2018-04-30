17fb3642ca07cddfbd8b447bc8d952c5f4f7c214
SeaCrap <2902933052@qq.com>
2018-04-30 16:36:51 +0800

    无缝轮播

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
-let n 
-inrize()
-setInterval(() => {
-	// $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-	leaveActive(indexImg(n)) //leaveActive这个函数返回值是undefined，
-		.one('transitionend',(e) => { // undefined后边接事件肯定不行，可以让leaveActive把这个节点当做返回值返回
-			// $(e.currentTarget).removeClass('leave').addClass('enter')
-			enterActive($(e.currentTarget))
-		})
-	// $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
-	currentActive(indexImg(n+1))
-	n += 1
-},2000)
+// 获取所有button
+let $buttons = $('#buttonsWrapper>button')
 
+let $slides = $('#slides') 
 
+let $images = $slides.children('img')
+let $firstCopy = $images.eq(0).clone(true) 
 
-//封装函数
+let $lastCopy = $images.eq($images.length-1).clone(true) 
 
-// 限制n/n+1在1,2,3,范围内
-function x(n){	
-	if(n > 3){
-		n = n%3
-		if(n === 0){
-			n = 3
-		}
+$slides.append($firstCopy) 
+$slides.prepend($lastCopy) 
+
+$slides.css({transform:'translateX(-400px)'})
+
+let current = 0 
+
+$buttons.eq(0).on('click',function(){
+	if(current === 2){
+		console.log('说明是从最后一张到第一张')
+		$slides.css({transform:'translateX(-1600px)'}) 
+			.one('transitionend',function(){
+				$slides.hide()
+					.offset()
+				$slides.css({transform:'translateX(-400px)'})
+					.show()
+			})
+	}else{
+		$slides.css({transform:'translateX(-400px)'})
 	}
-	return n
-} //n只能等于1，2,3
-
-//初始化
-function inrize(){
-	n = 1
-	$(`.images > img:nth-child(${n})`).addClass('current')
-	.siblings().addClass('enter') //第一个current状态，其他所有兄弟全部在enter状态
-	
-}
-//current状态
-function currentActive($node){
-	$node.removeClass('enter leave').addClass('current')
-}
-//leave状态
-function leaveActive($node){
-	$node.removeClass('current enter').addClass('leave')
-	return $node //返回节点 这样返回值就不是undefined了.或则直接return $node.removeClass('current enter').addClass('leave')
-}
-//enter状态
-function enterActive($node){
-	$node.removeClass('current leave').addClass('enter')
-}
-//选择器优化
-function indexImg(n){
-	return $(`.images > img:nth-child(${x(n)})`)
-}
+	current= 0
+})
+$buttons.eq(1).on('click',function(){
+	console.log(current)
+	$slides.css({transform:'translateX(-800px)'})
+	current= 1 
+})
+$buttons.eq(2).on('click',function(){
+	if(current === 0){
+		console.log('说明是从第一张到最后一张的')
+		$slides.css({transform:'translateX(0px)'})
+			.one('transitionend',function(){
+				$slides.hide()
+					.offset()
+				$slides.css({transform:'translateX(-1200px)'})
+					.show() 
+			})
+		}else{
+			$slides.css({transform:'translateX(-1200px)'})
+		
+		}
+		current= 2 
+})
